21e32aa573ff7c900bee211c234812c11b759e49
IanCG17 <134028152+IanCG17@users.noreply.github.com>
2023-07-06 13:40:43 -0500

    v.examen
    Tuve que organizar todo debido a lo mal organizado que lo había dejado

---
+++ DOM/talller/script.js
@@ -0,0 +1,9 @@
+function mostrarImagen() {
+    document.getElementById('imagen').style.display = 'block';
+    document.getElementById('nada').style.display = 'none';
+}
+
+function ocultarImagen() {
+    document.getElementById('imagen').style.display = 'none';
+    document.getElementById('nada').style.display = 'block';
+}
